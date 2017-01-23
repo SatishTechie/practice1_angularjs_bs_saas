8425d3c3b8d7f54cc6b416d0b457441b0ef6fa38
Satish <pawars029@gmail.com>
2017-01-23 13:03:28 +0530

    new files added

---
+++ app/scripts/app.js
@@ -0,0 +1,20 @@
+'use strict';
+angular
+  .module('app', [
+    'ngAnimate',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
